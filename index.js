378fdbcce7f9f8a24916f105a2704b5d73218915
Panoz <panozfc@gmail.com>
2025-04-14 20:45:10 +0200

    index back

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const pool = require("./db");
 app.use(express.json());
 app.use(express.static("public"));
 // Fallback su index.html se nessun'altra route viene trovata
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+
 
 
 const uploadResultsRoute = require("./uploadResults"); // Assicurati che il percorso sia corretto
